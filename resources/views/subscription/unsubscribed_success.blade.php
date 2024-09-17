80b5e3ece64b0d6fdf30d238f22ca80007bef85a
WaelHamwi <153142937+WaelHamwi@users.noreply.github.com>
2024-09-17 03:53:07 +0300

    Update unsubscribed_success.blade.php

--- resources/views/subscription/unsubscribed_success.blade.php
+++ resources/views/subscription/unsubscribed_success.blade.php
@@ -1,21 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Unsubscribed Successfully</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/3.2.4/tailwind.min.css">
-</head>
+@section('title', 'Weather App - Welcome')
 
-<body class="bg-gray-100">
-    <div class="container mx-auto p-4">
-        <div class="bg-white shadow-md rounded-lg p-6 text-center">
-            <h1 class="text-2xl font-bold mb-4">Unsubscribed Successfully</h1>
-            <p class="mb-6">You have successfully unsubscribed from our service. We hope to see you again soon!</p>
-            <a href="/" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">Back to Home</a>
-        </div>
+@section('content')
+<div class="container mx-auto p-4 min-h-screen">
+    <div class="bg-white shadow-md rounded-lg p-6 text-center ">
+        <h1 class="text-2xl font-bold mb-4 bg-red-600">Unsubscribed Successfully</h1>
+        <p class="mb-6 bg-red-600">You have successfully unsubscribed from our service. We hope to see you again soon!</p>
+        <a href="/" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">Back to Home</a>
     </div>
-</body>
-
-</html>
+</div>
+@endsection
